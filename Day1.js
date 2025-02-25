4463ee90277c6a18088cbbe94afc44b25c8c2d9b
Tirtayuda12 <tyuda3851@gmail.com>
2025-02-25 19:03:11 +0700

    Day1 update variable

--- Day1.js
+++ Day1.js
@@ -1,1 +1,34 @@
+//Tipe data string
+document.writeln('Hello World');
+document.writeln("</br>");
+//Tipe data number
+document.writeln(100);
+document.writeln("</br>");
+//Tipe data float
+document.writeln(100.123);
+document.writeln("</br>");
+//Tipe data biner
+document.writeln(0b110110);
+document.writeln("</br>");
+//Tipe data boolean
+document.writeln(true);
+document.writeln("</br>");
+document.writeln(false);
+document.writeln("</br>");
 
+//Javascript mendukung escape sequence
+document.writeln("<textarea cols='100' rows='10'>");
+document.writeln("Tirtayuda \nMunggarana");
+document.writeln("\\Home\\Tirtayuda");
+document.writeln("\"Programmer zaman now\"");
+document.writeln('\'Belajar Javascript\'');
+document.writeln("</textarea>");
+
+//Variable
+var fullName;
+var firstName;
+var lastName;
+
+fullName ="Tirtayuda Munggarana";
+firstName = "Tirtayuda";
+lastName = "Munggarana";
